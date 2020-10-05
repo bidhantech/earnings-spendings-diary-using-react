69e46c8d56e86c1cabee92e401d24493f779c29b
BIDHAN <53926488+bidhantech@users.noreply.github.com>
2020-10-05 19:46:20 +0530

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function List({ data }) {
+function List({ remove , data }) {
 
         data.sort((a,b)=>{
                 return new Date(b.date) - new Date(a.date);
@@ -9,17 +9,21 @@ function List({ data }) {
         
         return <ul className="List">
 
+                
                 <li>
                         <span className="date title"> Date </span>
                         <span className="detail title"> Details </span>
                         <span className="earn title"> Amount </span>
+                        <span className="delete title"> Delete </span>
                 </li>
 
+        
                 {data.map((t) =>
                         <li key={t.date}>
                                 <span className="date"> {t.date} </span>
                                 <span className="detail"> {t.details} </span>
                                 <span className={t.select_type}>{t.select_type==='spend'?'-':''}{t.amount}</span>
+                                <span className="delete" onClick={()=>remove(t.date)}> X </span>
                         </li>
                 )
                 }
